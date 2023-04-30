0fe42f15582abe53f77569ac2042a6bf3459b0b6
Joaquin Moreno <60682720+joaquinm683@users.noreply.github.com>
2023-04-30 11:21:23 -0500

    Proyecto con JDBC, springboot y db placeholder

---
+++ proyectogrupo5/src/main/java/pe/isil/proyectogrupo5/Proyectogrupo5Application.java
@@ -0,0 +1,13 @@
+package pe.isil.proyectogrupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectogrupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectogrupo5Application.class, args);
+	}
+
+}
